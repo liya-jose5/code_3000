2e38b9b4ca492f80fb3c1bce5bc4560cf133f26e
ljose <115601097+liya-jose5@users.noreply.github.com>
2026-09-17 16:00:57 -0400

    update build and test files

--- mod02_build_bot_predictor.py
+++ mod02_build_bot_predictor.py
@@ -10,9 +10,9 @@ def train_model(X, y, seed=seed):
     Build a GBM on given data
     """
     model = GradientBoostingClassifier(
-        learning_rate=0.4,
+        learning_rate=0.1,
         n_estimators=100,
-        max_depth=10,
+        max_depth=1,
         subsample=1,
         min_samples_leaf=1,
         random_state=seed
